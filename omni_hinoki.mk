8ce3d1b89e6dbd99a5e6cf723f2d53f72614ce2d
jmpfbmx <jmpf.bmx@gmail.com>
2022-01-19 17:28:34 +0100

    hinoki: Initial TWRP tree
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Ia631621acf01c4440fde0ed1af4c9c868d9c6b6b

---
+++ omni_hinoki.mk
@@ -0,0 +1,18 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := Sony
+PRODUCT_DEVICE := hinoki
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_MODEL := XPERIA XA1
+PRODUCT_NAME := omni_hinoki
+
+# enable stock zip packages flash
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.secure=1 \
+    ro.adb.secure=0 \
+    ro.allow.mock.location=0
